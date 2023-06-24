8130b63e25f489f86f3d6b318af099b1c143b2c1
Hanminho <hihi6024@gmail.com>
2023-06-25 07:27:56 +0900

    commit

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -9,8 +9,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 import logging
-from django.shortcuts import render,get_object_or_404
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
-from polls.models import Question,Choice
+
+from polls.models import Question, Choice
 
 logger = logging.getLogger(__name__)
-# Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -17,28 +18,33 @@ class IndexView(generic.ListView):
         # 최근 생성된 질문 5개를 반환함
         return Question.objects.all().order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
 
 class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def vote(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     logger.fatal('vote')
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except(KeyError,Choice.DoesNotExist):
-        return render(request,'polls/detail.html',{
-            'question':question,'error_message':"You did\'t select a choice.",
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
