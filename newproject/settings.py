8f2e43f8198c2738b1c554368fa5cfe6a7f7e6e8
Hanminho <hihi6024@gmail.com>
2023-06-25 23:53:29 +0900

    polls app 초기설정

--- newproject/settings.py
+++ newproject/settings.py
@@ -25,12 +25,15 @@ SECRET_KEY = 'django-insecure-p1juk0^19+53ko76zbj_c7*0k&i&6-8ysw#@26dl7f%kspp^_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ' localhost','127.0.0.1'
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
