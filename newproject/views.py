36f3659437f7ea1f1bef795fd4248b68f609bafa
Hanminho <hihi6024@gmail.com>
2023-06-25 19:19:38 +0900

    commit

--- newproject/views.py
+++ newproject/views.py
@@ -1,5 +1,7 @@
 import logging
+
 from django.views.generic import TemplateView
+
 from django.apps import apps
 
 class HomeView(TemplateView):
@@ -7,10 +9,11 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # context['app_list'] = ['polls', 'books']
         dictVerbose = {}
         for app in apps.get_app_configs():
             logging.getLogger('TEST').fatal(app.path)
             if 'site-packages' not in app.path:
-                dictVerbose['app/label'] = app.verbose_name
-            context['verbose_dict'] = dictVerbose
-            return context
+                dictVerbose[app.label] = app.verbose_name
+        context['verbose_dict'] = dictVerbose
+        return context
