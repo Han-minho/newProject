a91a85243ca40d8d6db47f76ec67957ede2e3602
Hanminho <hihi6024@gmail.com>
2023-06-25 12:15:07 +0900

    commit

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path("", views.IndexView.as_view(), name='index'),
-    path("<int:pk>/", views.DetailView.as_view(), name='detail'),
-    path("<int:question_id>/vote/", views.vote, name='vote'),
-    path("<int:pk>/results/", views.ResultView.as_view(), name='results'),
+    path("",views.IndexView.as_view(), name='index'),
+    path("<int:pk>/",views.DetailView.as_view(), name='detail'),
+    path("<int:question_id>/vote/",views.vote, name='vote'),
+    path("<int:pk>/results/",views.ResultView.as_view(), name='results'),
 ]
